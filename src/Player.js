77eb1893a33d34461997cc510467a35af19047f6
akhadka21 <akhadka2@ucsc.edu>
2026-06-12 17:59:12 -0700

    timers and powerups

--- src/Player.js
+++ src/Player.js
@@ -1,35 +1,67 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
-        super(scene, x, y, 'ships', 'spaceShips_001.png');
+        super(scene, x, y, 'ship1');
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.setCollideWorldBounds(true);
-        this.setScale(0.45);
-        this.body.setSize(42, 32, true);
+        this.setScale(0.75);
+        // this.body.setSize(42, 32, true);
+        this.body.setAllowGravity(false);
+        this.setDepth(90)
+
+        
+        this.maxHP = 100
+        this.hp = this.maxHP
 
         this.bullets = scene.physics.add.group();
-        this.lastFired = 0
-        this.fireRate = 200
+        this.lastFired = 0;
+        this.fireRate = 300;
+
+        // this.speed = 320;
+        
+        this.doubleShot = false;
+        this.speedBoost = false;
+
+        this.invuln = false;
+        this.lastInvuln = 0;
+        this.invulnCd = 10000;
+
+                     
+        this.keyW = this.scene.input.keyboard.addKey('W');  
+        this.keyA = this.scene.input.keyboard.addKey('A');
+        this.keyS = this.scene.input.keyboard.addKey('S');
+        this.keyD = this.scene.input.keyboard.addKey('D');
+
+        this.shiftKey = this.scene.input.keyboard.addKey('SHIFT');
+   
+
     }
     
+    create(){
+        
+    }
 
     getPlayerPos(){
         return {xpos: this.x, ypos: this.y}
     }
 
     update(cursors, spaceKey, time) {
-        if (cursors.left.isDown) {
-            this.setVelocityX(-320);
-        } else if (cursors.right.isDown) {
-            this.setVelocityX(320);
+
+
+        let speed = this.speedBoost ? 640 : 320;
+
+        if (this.keyA.isDown) {
+            this.setVelocityX(-speed);
+        } else if (this.keyD.isDown) {
+            this.setVelocityX(speed);
         } else {
             this.setVelocityX(0);
         }
 
-        if (cursors.up.isDown) {
-            this.setVelocityY(-320);
-        } else if (cursors.down.isDown) {
-            this.setVelocityY(320);
+        if (this.keyW.isDown) {
+            this.setVelocityY(-speed);
+        } else if (this.keyS.isDown) {
+            this.setVelocityY(speed);
         } else {
             this.setVelocityY(0);
         }
@@ -37,12 +69,59 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         if (spaceKey.isDown && time > this.lastFired + this.fireRate) {
             this._fire(time);
         }
+
+        if (spaceKey.isDown && time > this.lastFired + this.fireRate) {
+            this._fire(time);
+        }
+
+        if (this.shiftKey.isDown && time > this.lastInvuln + this.invulnCd) {
+            this.setInvin(time);
+        }
+
+
+
     }
 
     _fire(time) {
         this.lastFired = time
-        const b = this.bullets.create(this.x, this.y - 24, 'ships', 'spaceMissiles_001.png');
-        b.setVelocityY(-520);
-        b.setScale(0.9);
+        
+        if (this.doubleShot){
+            const a = this.bullets.create(this.x - 5, this.y - 24, 'ships', 'spaceMissiles_001.png');
+            a.setVelocityY(-520);
+            a.setScale(0.4);
+
+            const b = this.bullets.create(this.x + 5, this.y - 24, 'ships', 'spaceMissiles_001.png');
+            b.setVelocityY(-520);
+            b.setScale(0.4);
+        } else {
+            const b = this.bullets.create(this.x, this.y - 24, 'ships', 'spaceMissiles_001.png');
+            b.setVelocityY(-520);
+            b.setScale(0.4);
+        }
     }
-}
+
+    takeDamageP(amount){
+        if (!this.invuln){
+            this._flashHurt();
+            this.hp -= amount;
+        }
+    }
+
+    setInvin(time){
+        console.log("hi");
+        this.lastInvuln = time
+        this.invuln = true;
+        this.setTint(0x34e5eb);
+        this.scene.time.delayedCall(700, () => {
+            this.invuln = false;
+            if (this.active) this.clearTint();
+        });
+    }
+
+    _flashHurt() {
+        this.setTint(0xff3333);
+        this.scene.time.delayedCall(160, () => {
+            if (this.active) this.clearTint();
+        });
+    }
+}   
--- src/Scenes/BossBattle.js
+++ src/Scenes/BossBattle.js
@@ -6,7 +6,13 @@ class BossBattle extends Phaser.Scene {
     preload() {
         // this.load.atlasXML('eyeboss', 'assets/character_eyeboss_sheet.png', 'assets/character_eyeboss_sheet.xml');
         this.load.atlasXML('ships', 'assets/spaceShooter2_spritesheet.png', 'assets/spaceShooter2_spritesheet.xml');
+        for (let i = 1; i <= 4; i++){
+            this.load.image(`ship${i}`,
+                `assets/player/ship${i}.png`
+            );
+        }
 
+        
         for (let i = 1; i <= 3; i++) {
             this.load.image(`boss_${i}`, 
                 `assets/bosseye/EyeBoss-Sheet${i}.png`
@@ -64,9 +70,9 @@ class BossBattle extends Phaser.Scene {
             this.stars.push(this.add.rectangle(x, y, sz, sz, 0xffffff).setAlpha(Phaser.Math.FloatBetween(0.2, 0.8)));
         }
 
-            this._createAnimations();
+        this._createAnimations();
 
-        this.createFireSlash(0, 0 , 30, 100);
+
 
         
 
@@ -78,12 +84,19 @@ class BossBattle extends Phaser.Scene {
         this.bosseye.flipY = true;
         
         this.flames = [];
+        this.powers = [];
+
+        this.powerUpTimers = {};
+        this.powerUpGraphics = this.add.graphics().setDepth(200).setScrollFactor(0);
+        this.ShieldGraphics =  this.add.graphics().setDepth(200).setScrollFactor(0);
 
         this.boss.flipY = true;
         this.boss.setScale(2);
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.rKey = this.input.keyboard.addKey('R');
+
 
         this.physics.add.overlap(this.player.bullets, this.boss, (boss, bullet) => {
             bullet.destroy();
@@ -92,17 +105,38 @@ class BossBattle extends Phaser.Scene {
 
         this.physics.add.overlap(this.boss.bullets, this.player, (player, bullet) => {
             bullet.destroy();
+            player.takeDamageP(7);
         });
 
         // HUD
-        this.hpGraphics = this.add.graphics().setDepth(10);
+        this.hpBossGraphics = this.add.graphics().setDepth(10);
         this.add.text(10, 8, 'BOSS HP', {
             fontSize: '12px', color: '#aaaacc', fontFamily: 'monospace'
         }).setDepth(10);
         this.stateLabel = this.add.text(790, 10, 'STATE: amused', {
             fontSize: '14px', color: '#ffee00', fontFamily: 'monospace'
         }).setOrigin(1, 0).setDepth(10);
-        this.add.rectangle(400, 46, 800, 1, 0x334466).setDepth(10);
+        // this.add.rectangle(400, 46, 800, 1, 0x334466).setDepth(10);
+        
+        this.hpPlayerGraphics = this.add.graphics().setDepth(10);
+        this.add.text(10, 560, 'PLAYER HP', {
+            fontSize: '12px', color: '#aaaacc', fontFamily: 'monospace'
+        }).setDepth(10);
+        // this.stateLabel = this.add.text(790, 10, 'STATE: amused', {
+        //     fontSize: '14px', color: '#ffee00', fontFamily: 'monospace'
+        // }).setOrigin(1, 0).setDepth(10);
+
+        this.add.rectangle(400, 300, 800, 600).setStrokeStyle(4, 0x5566aa).setDepth(100);
+
+
+        this.createFireSlash(400, 0 , 90, 10, 75);
+
+        // this.createPowerUp(100, 300, 1);
+        // this.createPowerUp(150, 300, 2);
+        // this.createPowerUp(200, 300, 3);
+        this.createPowerUp(250, 300);
+        this.createPowerUp(350, 300);
+
     }
 
     _createAnimations() {
@@ -136,30 +170,56 @@ class BossBattle extends Phaser.Scene {
     update(time, delta) {
         this.player.update(this.cursors, this.spaceKey, time);
         this.boss.update(time, delta);
-        this._updateHPBar();
+        this._updateBossHPBar();
+        this._updatePlayerHPBar();
         this._cullBullets();
         this.scrollStarField(delta);
         this.deleteOffscreenStars();
         this.addNewStars();
         this.bosseye.setPosition(this.boss.x, this.boss.y + 20);
         this.pulse(time);
+        this.updateCollision();
+        this._drawPowerUpTimers();
+        this._drawShieldUpTimers()
+
+        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
+            this.scene.restart();
+        }
     }
 
-    _updateHPBar() {
+    _updateBossHPBar() {
         const pct = this.boss.hp / this.boss.maxHP;
         const barX = 10;
         const barY = 24;
         const barW = 300;
         const barH = 16;
 
-        this.hpGraphics.clear();
-        this.hpGraphics.fillStyle(0x1a1a33);
-        this.hpGraphics.fillRect(barX, barY, barW, barH);
+        this.hpBossGraphics.clear();
+        this.hpBossGraphics.fillStyle(0x1a1a33);
+        this.hpBossGraphics.fillRect(barX, barY, barW, barH);
         const color = pct > 0.6 ? 0x22cc44 : pct > 0.3 ? 0xffcc00 : 0xff3300
-        this.hpGraphics.fillStyle(color);
-        this.hpGraphics.fillRect(barX, barY, barW * pct, barH);
-        this.hpGraphics.lineStyle(1, 0x5566aa);
-        this.hpGraphics.strokeRect(barX, barY, barW, barH);
+        this.hpBossGraphics.fillStyle(color);
+        this.hpBossGraphics.fillRect(barX, barY, barW * pct, barH);
+        this.hpBossGraphics.lineStyle(1, 0x5566aa);
+        this.hpBossGraphics.strokeRect(barX, barY, barW, barH);
+    }
+
+    
+    _updatePlayerHPBar() {
+        const pct = this.player.hp / this.player.maxHP;
+        const barX = 10;
+        const barY = 576;
+        const barW = 300;
+        const barH = 16;
+
+        this.hpPlayerGraphics.clear();
+        this.hpPlayerGraphics.fillStyle(0x1a1a33);
+        this.hpPlayerGraphics.fillRect(barX, barY, barW, barH);
+        const color = pct > 0.6 ? 0x22cc44 : pct > 0.3 ? 0xffcc00 : 0xff3300
+        this.hpPlayerGraphics.fillStyle(color);
+        this.hpPlayerGraphics.fillRect(barX, barY, barW * pct, barH);
+        this.hpPlayerGraphics.lineStyle(1, 0x5566aa);
+        this.hpPlayerGraphics.strokeRect(barX, barY, barW, barH);
     }
 
     _cullBullets() {
@@ -175,21 +235,188 @@ class BossBattle extends Phaser.Scene {
         if (this.stateLabel) this.stateLabel.setText('STATE: ' + name);
     }
 
-    createFireSlash(x, y, angle, duration) {
+    createFireSlash(x, y, angle, amount, gap) {
         let offset = Phaser.Math.DegToRad(angle);
-        for (let i = 0; i < 10; i++) {
-            let flameX = x + Math.cos(offset) * (i * 120);
-            let flameY = y + Math.sin(offset) * (i * 120);
-            let flame = this.add.sprite(flameX, flameY, "fire1");
-            flame.play("fire-crackle");
-            // this.flames.push(flame);
+        for (let i = 0; i <= amount; i++) {
+            let flameX = x + Math.cos(offset) * (i * gap);
+            let flameY = y + Math.sin(offset) * (i * gap);
 
-            flame.on('animationcomplete', () => {
-                flame.destroy();
+            let warning = this.add.circle(flameX, flameY, 5, 0xff0000, 0.6);
+            this.tweens.add({
+                targets: warning,
+                scaleX: 3,
+                scaleY: 3,
+                alpha: 0.3,
+                duration: 1500,
+                ease: 'Sine.easeIn',
+                onComplete: () => {
+                    warning.destroy();
+
+                    let flame = this.physics.add.sprite(flameX, flameY, "fire1");
+                    flame.body.setAllowGravity(false);
+                    flame.setScale(.5);
+                    flame.play("fire-crackle");
+                    this.flames.push(flame);
+
+                    flame.on('animationcomplete', () => {
+                        flame.destroy();
+                        this.flames = this.flames.filter(f => f.active);
+                    });
+                }
             });
         }
     }
 
-    
+    createPowerUp(x, y, type) {
+        const types = ['health', 'double', 'speed'];
+        const colors = { health: 0x32a852, double: 0xa832a0, speed: 0x3278a8 };
+        const chosen = type ?? Phaser.Utils.Array.GetRandom(types);
 
+        let pu = this.add.circle(x, y, 10, colors[chosen]);
+        pu.setDepth(50);
+        pu.powerType = chosen;
+
+        this.physics.add.existing(pu);
+        pu.body.setAllowGravity(false);
+        pu.body.setCircle(10);
+
+        this.powers.push(pu);
+
+        this.tweens.add({
+            targets: pu,
+            y: y - 10,
+            duration: 800,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sine.easeInOut'
+        });
+    }
+
+    updateCollision(){
+        this.collisionFire();
+        this.collisionPowerup();
+    }
+
+    collisionPowerup(){
+        this.powers = this.powers.filter(pu => {
+        
+        if (!pu.active) {
+            return false;
+        }
+
+        if (Phaser.Geom.Intersects.RectangleToRectangle(
+            this.player.getBounds(), pu.getBounds()
+        )) {
+            if (pu.powerType === 'health') {
+                this.player.hp = Math.min(this.player.maxHP, this.player.hp + 20);
+            } else if (pu.powerType === 'double') {
+                this.player.doubleShot = true;
+                this.powerUpTimers['double'] = { startTime: this.time.now, duration: 5000 };
+                this.time.delayedCall(5000, () => {
+                    this.player.doubleShot = false;
+                    delete this.powerUpTimers['double'];
+                });
+            } else if (pu.powerType === 'speed') {
+                this.player.speedBoost = true;
+                this.powerUpTimers['speed'] = { startTime: this.time.now, duration: 5000 };
+                this.time.delayedCall(5000, () => {
+                    this.player.speedBoost = false;
+                    delete this.powerUpTimers['speed'];
+                });
+            }
+            pu.destroy();
+            return false;
+        }
+        return true;
+       });
+    }
+
+    collisionFire() {
+        this.physics.add.overlap(this.player, this.flames, (obj1, obj2) => {
+            obj2.destroy();
+            this.flames = this.flames.filter(f => f.active);
+            console.log("hi");
+            obj1.takeDamageP(5);
+            if (this.pauseParticles) {
+                this.time.delayedCall(500, () => {
+                    this.scene.pause();
+                    setTimeout(() => this.scene.resume(), 3000);
+                    this.pauseParticles = false;
+                });
+            }
+        });
+    }
+
+    _drawPowerUpTimers() {
+    this.powerUpGraphics.clear();
+
+    const icons = { double: 0xa832a0, speed: 0x3278a8 };
+    let slot = 0;
+
+    for (const [type, data] of Object.entries(this.powerUpTimers)) {
+        const x = 700 + slot * 50;
+        const y = 575;
+        const radius = 16;
+        const pct = 1 - (this.time.now - data.startTime) / data.duration;
+
+        if (pct <= 0) {
+            delete this.powerUpTimers[type];
+            continue;
+        }
+
+        this.powerUpGraphics.fillStyle(0x222222, 0.8);
+        this.powerUpGraphics.fillCircle(x, y, radius);
+
+        this.powerUpGraphics.fillStyle(icons[type], 0.9);
+        this.powerUpGraphics.slice(x, y, radius, -Math.PI / 2, -Math.PI / 2 + pct * Math.PI * 2, false);
+        this.powerUpGraphics.fillPath();
+
+        this.powerUpGraphics.lineStyle(2, 0xffffff, 0.6);
+        this.powerUpGraphics.strokeCircle(x, y, radius);
+
+        slot++;
+    }
+
+    
+    }
+    
+    _drawShieldUpTimers() {
+        
+        console.log(this.player.lastInvuln)
+        console.log(this.player.invulnCd)
+        console.log(this.time)
+        console.log("------")
+
+        
+        
+
+        if(this.player.lastInvuln + this.player.invulnCd < this.time.now){
+            // this.ShieldGraphics.clear();
+            return;
+        }
+
+        this.ShieldGraphics.clear();
+
+        const invuln =  0x34e5eb;
+
+        
+        const x = 26;
+        const y = 540;
+        const radius = 16;
+        const pct = 1 - (this.time.now - this.player.lastInvuln) / this.player.invulnCd;
+
+
+        this.ShieldGraphics.fillStyle(0x222222, 0.8);
+        this.ShieldGraphics.fillCircle(x, y, radius);
+
+        this.ShieldGraphics.fillStyle(invuln, 0.9);
+        this.ShieldGraphics.slice(x, y, radius, -Math.PI / 2, -Math.PI / 2 + pct * Math.PI * 2, false);
+        this.ShieldGraphics.fillPath();
+
+        this.ShieldGraphics.lineStyle(2, 0xffffff, 0.6);
+        this.ShieldGraphics.strokeCircle(x, y, radius);
+
+
+        
+    }
 }
